0a7aec91148d2a8a476db5bdc77c00100270ba92
admin <shaikh96arshad@gmail.com>
2023-08-28 16:03:14 +0530

    Recipe DKU_TUTORIAL_BASICS_101.compute_test_1 saved through notebook notebook editor for compute_test_1

--- recipes/compute_test_1.py
+++ recipes/compute_test_1.py
@@ -1,3 +1,4 @@
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 # -*- coding: utf-8 -*-
 import dataiku
 import pandas as pd, numpy as np
@@ -12,9 +13,13 @@ covid19___orignal_df = covid19___orignal.get_dataframe()
 # TODO: Replace this part by your actual code that computes the output, as a Pandas dataframe
 # NB: DSS also supports other kinds of APIs for reading and writing data. Please see doc.
 
-test_1_df = covid19__orignal_df.head(3)
+# test_1_df = covid19__orignal_df.head(3)
 
-test_1_df = covid19___orignal_df # For this sample code, simply copy input to output
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+test_1_df = covid19___orignal_df.head(3)
+
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+# test_1_df = covid19___orignal_df # For this sample code, simply copy input to output
 
 
 # Write recipe outputs
